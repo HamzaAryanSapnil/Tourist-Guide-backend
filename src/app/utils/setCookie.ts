1ce8236818a2982fd12958bfa1847312c252810c
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-09-11 14:27:24 +0600

    working with setCookie

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { envVars } from "../config/env";
 // import { envVars } from "../config/env";
 
 export interface AuthTokens {
@@ -10,16 +11,18 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     if (tokenInfo.accessToken) {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
-            secure: true ,
-            sameSite: "none"
+            secure: envVars.NODE_ENV === "production",
+            sameSite:envVars.NODE_ENV === "production"? "none": "lax",
+            path: "/",
         })
     }
 
     if (tokenInfo.refreshToken) {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
           httpOnly: true,
-          secure: true,
-          sameSite: "none",
+          secure: envVars.NODE_ENV === "production",
+          sameSite: envVars.NODE_ENV === "production" ? "none" : "lax",
+          path: "/",
         });
     }
 }
